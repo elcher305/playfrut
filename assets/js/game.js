9661c3603f956ff53031032de90821037a793b7f
elcher305 <elvira.chernova.05@gmail.com>
2024-09-29 19:36:27 +0700

    game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -30,8 +30,60 @@ class Drawable {
             height: ${this.h}px;
         `;
     }
+
+    isCollision(element) {
+        let a = {
+            x1: this.x,
+            y1: this.y,
+            x2: this.x + this.w,
+            y2:this.y + this.h,
+        }
+        let b = {
+            x1: element.x,
+            y1: element.y,
+            x2: element.x + element.w,
+            y2:element.y + element.h,
+        }
+        return a.x1 < b.x2 && b.x1 < a.x2 && a.y1 < b.y2 && b.y1 < a.y2;
+    }
+
 }
 
+class Fruit extends Drawable {
+    constructor(game) {
+        super(game);
+        this.w = 70;
+        this.h = 70;
+        this.y = 60;
+        this.x = random(0, window.innerWidth - this.w);
+        this.offsets.y = 3;
+        this.createElement();
+    }
+    update() {
+        if(this.isCollision(this.game.player)) console.log('col')
+        super.update();
+    }
+}
+
+class Banana extends Fruit {
+    constructor(game) {
+        super(game);
+    }
+}
+
+class Apple extends  Fruit {
+    constructor(game) {
+        super(game);
+        this.offsets.y = 5;
+    }
+}
+
+class Orange extends  Fruit {
+    constructor(game) {
+        super(game);
+        this.offsets.y = 7;
+    }
+}
 class Player extends Drawable {
     constructor(game) {
         super(game);
@@ -39,15 +91,43 @@ class Player extends Drawable {
         this.h = 109;
         this.x = window.innerWidth / 2 - this.w / 2;
         this.y = window.innerHeight - this.h;
+        this.speedPerFrame = 20;
+        this.keys = {
+            ArrowLeft: false,
+            ArrowRight: false
+        };
         this.createElement();
+        this.bindKeyEvents();
     }
+
+    bindKeyEvents() {
+        document.addEventListener('keydown', ev => this.changeKeyStatus(ev.code, true))
+        document.addEventListener('keyup', ev => this.changeKeyStatus(ev.code, false))
+    }
+
+    changeKeyStatus(code, value) {
+        if(code in this.keys) this.keys[code] = value;
+     }
+
+     update() {
+        if(this.keys.ArrowLeft && this.x > 0) {
+            this.offsets.x = -this.speedPerFrame;
+        } else if(this.keys.ArrowRight && this.x < window.innerWidth - this.w) {
+            this.offsets.x = this.speedPerFrame;
+        } else {
+            this.offsets.x = 0;
+        }
+         super.update();
+     }
 }
 
 class Game {
     constructor() {
         this.name = name;
         this.elements = [];
-        this.players = this.generate(Player);
+        this.player = this.generate(Player);
+        this.counterForTimer = 0;
+        this.fruits = [Apple, Banana, Orange];
     }
 
     generate(className) {
@@ -62,11 +142,20 @@ class Game {
 
     loop() {
         requestAnimationFrame(() => {
+            this.counterForTimer++;
+            if(this.counterForTimer % 60 === 0){
+                this.randomFruitGenerate();
+            }
             this.updateElement();
             this.setParams();
             this.loop();
         });
     }
+
+    randomFruitGenerate() {
+        this.generate(this.fruits[random(0, 2)])
+    }
+
 
     updateElement() {
         this.elements.forEach(element => {
